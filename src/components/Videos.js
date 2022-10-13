b25400d80d806d0698b48dce4c8b54c4a1465cef
RonnyKn <ronnykurniawn@gmail.com>
2022-10-13 22:29:35 +0700

    fix bugs

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -2,12 +2,20 @@ import { Box, Stack } from "@mui/material"
 import React from "react"
 import VideoCard from "./VideoCard"
 import ChannelCard from "./ChannelCard"
+import Loader from "./Loader"
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, index) => (
-        <Box key={item.index}>
+        <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
